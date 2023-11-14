dea8291fcdf3bbf9d8b83ca3d6737df159c7feab
Pasha <pashaie@gmail.com>
2023-11-14 20:44:38 +0330

    .

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -38,10 +38,26 @@ export default function PostList() {
   return (
     <>
       <h1>Post List {data.length}</h1>
-      <button className="btn btn-primary">OK</button>
-      <ul>
-        <li>...</li>
-      </ul>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">User Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d: any) => (
+            <tr>
+              <th scope="row">{d.id}</th>
+              <td>{d.userId}</td>
+              <td>{d.title}</td>
+              <td>{d.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
