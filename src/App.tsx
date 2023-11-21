4da27e7c28d269b92a5e337e71e2a040178608bc
Pasha <pashaie@gmail.com>
2023-11-21 21:30:10 +0330

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import PostIndex from "./pages/Post/PostIndex";
-import TaskList from "./components/TaskList/TaskList";
-import PhotoIndex from "./pages/Photo/PhotoIndex";
-import TodoIndex from "./pages/Todo/TodoIndex";
-import SelectColor from "./components/SelectColor";
 import { AppContext } from "./appContext";
+import AppLayout from "./components/AppLayout";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
   const [color, setColor] = useState("red");
   return (
-    <AppContext.Provider value={{ color, setColor }}>
-      <SelectColor></SelectColor>
-      <TaskList></TaskList>
-      <TodoIndex></TodoIndex>
-      <PhotoIndex></PhotoIndex>
-      <button onClick={() => setToggle(!toggle)}>toggle</button>
-      {toggle && <PostIndex></PostIndex>}
-
-      <h1>Task List</h1>
-    </AppContext.Provider>
+    <div dir="rtl">
+      <AppContext.Provider value={{ color, setColor }}>
+        <AppLayout></AppLayout>
+      </AppContext.Provider>
+    </div>
   );
 }
 
---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import SelectColor from "./SelectColor";
+import PhotoIndex from "../pages/Photo/PhotoIndex";
+import PostIndex from "../pages/Post/PostIndex";
+import TodoIndex from "../pages/Todo/TodoIndex";
+import TaskList from "./TaskList/TaskList";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <FileOutlined />),
+];
+
+const AppLayout: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: "0 16px" }}>
+          <SelectColor></SelectColor>
+          <TaskList></TaskList>
+          <TodoIndex></TodoIndex>
+          <PhotoIndex></PhotoIndex>
+          <button onClick={() => setToggle(!toggle)}>toggle</button>
+          {toggle && <PostIndex></PostIndex>}
+
+          <h1>Task List</h1>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default AppLayout;
