cdd7392916cff676b17c711e588c3aa5984d3547
Pasha <pashaie@gmail.com>
2023-11-21 19:28:48 +0330

    IIFE

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -7,20 +7,49 @@ export const useFetchData = <T>(endpoint: string) => {
   const [pageSize, setPageSize] = useState(10);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
+    //Promise
+    //Async
+    // fetch(
+    //   `https://jsonplaceholder.ir/${endpoint}?_limit=${pageSize}&_page=${page}`
+    // )
+    //   .then((resp) => {
+    //     const x = resp.headers.get("X-Total-Count") || "0";
+    //     setTotal(+x);
+    //     return resp.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     setData(data);
+    //     setLoading(false);
+    //   });
+
     setLoading(true);
-    fetch(
-      `https://jsonplaceholder.ir/${endpoint}?_limit=${pageSize}&_page=${page}`
-    )
-      .then((resp) => {
-        const x = resp.headers.get("X-Total-Count") || "0";
-        setTotal(+x);
-        return resp.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setData(data);
-        setLoading(false);
-      });
+    // const loadData = async () => {
+    //   //Async/Await
+    //   const resp = await fetch(
+    //     `https://jsonplaceholder.ir/${endpoint}?_limit=${pageSize}&_page=${page}`
+    //   );
+    //   const x = resp.headers.get("X-Total-Count") || "0";
+    //   setTotal(+x);
+    //   const data = await resp.json();
+    //   setData(data);
+    //   setLoading(false);
+    // };
+    // loadData();
+
+    //IIFE - Immediately Invoked Function Expression
+    (async () => {
+      //Async/Await
+      const resp = await fetch(
+        `https://jsonplaceholder.ir/${endpoint}?_limit=${pageSize}&_page=${page}`
+      );
+      const x = resp.headers.get("X-Total-Count") || "0";
+      setTotal(+x);
+      const data = await resp.json();
+      setData(data);
+      setLoading(false);
+    })();
+
     return () => {
       console.log("goodby! 👋");
     };
