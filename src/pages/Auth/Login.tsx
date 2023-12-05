011e58eb63df10253dc5bc15af37d47e6d2e01e0
Pasha <pashaie@gmail.com>
2023-12-05 20:02:19 +0330

    login

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+};
 
 export default function Login() {
-  return <div>Login</div>;
+  const onFinish = (values: FieldType) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 }
