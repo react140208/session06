a16f3f5d8e4b30aa57d0c45581a88c2d1ff89ae8
Pasha <pashaie@gmail.com>
2023-12-19 18:22:24 +0330

    error handling

--- src/core/appApi.ts
+++ src/core/appApi.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 // import { store } from "../redux/store";
+import { notification } from "antd";
 
 export const appApi = axios.create({
   baseURL: "http://localhost:3010",
@@ -13,3 +14,19 @@ appApi.interceptors.request.use(async (config) => {
   }
   return config;
 });
+
+appApi.interceptors.response.use(
+  (response) => {
+    return Promise.resolve(response);
+  },
+  (error) => {
+    console.log(error);
+    if (error.response?.data?.message) {
+      notification.error({ message: error.response?.data?.message });
+    }
+    if (error.response?.data?.error) {
+      notification.error({ message: error.response?.data?.error });
+    }
+    return Promise.reject(error);
+  }
+);
