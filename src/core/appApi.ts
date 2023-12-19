4164576f3718edde6f44402abba52e06c74ae47e
Pasha <pashaie@gmail.com>
2023-12-19 18:08:19 +0330

    request token interceptors

--- src/core/appApi.ts
+++ src/core/appApi.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
+// import { store } from "../redux/store";
 
 export const appApi = axios.create({
   baseURL: "http://localhost:3010",
+});
+
+appApi.interceptors.request.use(async (config) => {
+  const { store } = await import("../redux/store");
+  const token = store.getState().auth.token;
+  if (token) {
+    config.headers["token"] = token;
+  }
+  return config;
 });
